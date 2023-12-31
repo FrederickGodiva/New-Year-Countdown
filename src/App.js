f14eb15f2dfd7d9d9a37dfad35f345862d1d0534
DP5-Frederick-038 <godivafrederick@gmail.com>
2023-12-31 11:19:25 +0700

    Final

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
+import { useState } from "react";
 import Particles from "react-particles";
 import { loadFireworksPreset } from "tsparticles-preset-fireworks";
+import { Typewriter } from "react-simple-typewriter";
+import Countdown from "react-countdown";
 
 function App() {
-  const particleInitialization = async (engine) => {
-    await loadFireworksPreset(engine);
+  const [newYearMessage, setNewYearMessage] = useState(["Bye 2023👋"]);
+
+  const particlesInit = async (preset) => {
+    await loadFireworksPreset(preset);
+  };
+
+  const timeLeft = () => {
+    const newYearDate = new Date("January 1, 2024 00:00:00").getTime();
+    const now = new Date().getTime();
+    const difference = newYearDate - now;
+    return difference;
   };
 
   return (
     <>
-      <Particles
-        init={particleInitialization}
-        options={{ preset: "fireworks" }}
-      />
+      <Particles init={particlesInit} options={{ preset: "fireworks" }} />
+      <div className="flex justify-center items-center min-h-screen flex-col gap-4">
+        <span className="text-white font-bold text-4xl text-center px-4 z-50">
+          <Typewriter
+            words={newYearMessage}
+            loop={false}
+            cursor
+            cursorStyle={"_"}
+            typeSpeed={70}
+            deleteSpeed={50}
+            delaySpeed={1000}
+          />
+        </span>
+        <div className="text-white font-bold text-2xl z-50 ">
+          <Countdown
+            date={Date.now() + timeLeft()}
+            onComplete={() =>
+              setNewYearMessage(["Happy", "New", "Year", "✨2024✨"])
+            }
+          />
+        </div>
+      </div>
     </>
   );
 }
